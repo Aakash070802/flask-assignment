ccbb90e10672fd23dcf4f409b3f9a006eeb8aa4d
Aakash Kashyap <aakash.kash92@gmail.com>
2026-03-15 01:22:55 +0530

    feat: enhance form submission with additional fields and styling
    - Added static file serving for public assets.
    - Updated form in index.ejs to include fields for phone number, city, and message.
    - Enhanced success.ejs to display submitted details including name, email, phone, city, and message.
    - Created CSS styles for form and success pages to improve UI.
    - Introduced nodemon as a development dependency for easier server restarts.
    - Added .gitkeep file in public directory to ensure it is tracked by git.

--- backend/app.py
+++ backend/app.py
@@ -4,18 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    return 'Flask Server is running!!'
 
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form.get('name')
     email = request.form.get('email')
+    phone = request.form.get('phone')
+    city = request.form.get('city')
+    message = request.form.get('message')
 
     return jsonify({
         "status": "success",
         "name": name,
-        "email": email
+        "email": email,
+        "phone": phone,
+        "city": city,
+        "message": message
     })
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- frontend/app.js
+++ frontend/app.js
@@ -10,14 +10,15 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// const BACKEND_URL = "http://localhost:5000/submit" || process.env.BACKEND_URL;
-const BACKEND_URL =
-  process.env.BACKEND_URL || "http://65.2.179.138:5000/submit";
+const BACKEND_URL = "http://localhost:5000/submit";
+// const BACKEND_URL =
+//   process.env.BACKEND_URL;
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -33,7 +34,13 @@ app.post("/submit", async (req, res) => {
   const result = await response.json();
 
   if (result.status === "success") {
-    res.render("success", { name: result.name });
+    res.render("success", {
+      name: result.name,
+      email: result.email,
+      phone: result.phone,
+      city: result.city,
+      message: result.message,
+    });
   } else {
     res.send("Submission failed. Please try again.");
   }
